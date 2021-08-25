d0174a4c93909f7b1e418bb82acf98d1d272bcae
Chris DiPiero <frogmaster99@msn.com>
2021-08-24 23:50:26 -0500

    finished tilemap added path

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import Phaser from 'phaser';
 
+let graphics; // used in create to make path - remove when done
+let path; // used in create to make path
+
 class MyGame extends Phaser.Scene
 {
   constructor ()
@@ -16,22 +19,38 @@ class MyGame extends Phaser.Scene
   {
     //main grid
     const board = [
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      [7, 8, 9, 0, 10, 8, 7, 8, 8, 9, 7, 7, 8, 9, 6],
+      [6, 8, 9, 0, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
+      [7, 7, 7, 0, 7, 7, 7, 4, 1, 1, 1, 1, 5, 7, 7],
+      [7, 7, 7, 0, 7, 7, 7, 0, 7, 7, 7, 7, 0, 7, 7],
+      [7, 7, 7, 0, 7, 7, 7, 2, 1, 1, 5, 7, 0, 7, 7],
+      [7, 7, 7, 0, 7, 7, 7, 7, 7, 7, 0, 7, 0, 7, 7],
+      [7, 7, 7, 2, 1, 1, 1, 1, 1, 1, 3, 7, 0, 7, 7],
+      [7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 0, 7, 7],
+      [7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 0, 7, 7],
+      [7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 0, 7, 7]
     ]
 
     // populate the tiles
     const map = this.make.tilemap({ data: board,  tileWidth: 60, tileHeight: 60 });
     const tileset = map.addTilesetImage('metal.png', 'tiles', 60, 60);
     const layer = map.createLayer( 0, tileset, 0, 0 );
+
+    //make path
+    let graphics = this.add.graphics(); // temp to see path
+
+    path = this.add.path(210, -30);
+    path.lineTo(210, 390);
+    path.lineTo(630, 390);
+    path.lineTo(630, 270);
+    path.lineTo(450, 270);
+    path.lineTo(450, 150);
+    path.lineTo(750, 150);
+    path.lineTo(750, 630);
+    
+    graphics.lineStyle(3, 0xffffff, 1);
+    // visualize the path
+    path.draw(graphics);
   }
 }
 
